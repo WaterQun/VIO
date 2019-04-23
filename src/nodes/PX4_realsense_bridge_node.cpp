ee52c92863a4f297d08db9ee2ba039b1b0e531db
Martina Rivizzigno <martina@rivizzigno.it>
2019-04-23 13:52:39 +0200

    fix node: ros::time called befefore nodehandle, add camera serial number to launch file

--- src/nodes/PX4_realsense_bridge_node.cpp
+++ src/nodes/PX4_realsense_bridge_node.cpp
@@ -3,11 +3,8 @@
 using namespace bridge;
 
 int main(int argc, char** argv) {
-  ros::init(argc, argv, "PX4_realsense_bridge_node");
-
 
 	ros::init(argc, argv, "PX4_realsense_bridge_node");
-	ros::Rate loop_rate(10);
 	ros::NodeHandle nh("~");
 	PX4_Realsense_Bridge Brigde(nh);
 
