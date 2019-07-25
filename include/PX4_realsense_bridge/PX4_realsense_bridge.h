6927671b484b3a082d8b66c721ca33d7a737a085
kritz <kritz@ethz.ch>
2019-07-25 13:47:35 +0200

    Vio status (#3)
    * lower case name PX4 > px4
    
    * small fix
    
    * vio heartbeat
    
    * added flight termination case
    
    * initialize vio status with UNINIT
    
    * move publishSystemStatus to the node file

--- include/PX4_realsense_bridge/PX4_realsense_bridge.h
+++ include/PX4_realsense_bridge/PX4_realsense_bridge.h
@@ -2,16 +2,31 @@
 #define PX4_REALSENSE_BRIDGE
 
 #include <nav_msgs/Odometry.h>
+#include <mavros_msgs/CompanionProcessStatus.h>
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
 
 namespace bridge {
 
+  enum class MAV_STATE {
+  MAV_STATE_UNINIT,
+  MAV_STATE_BOOT,
+  MAV_STATE_CALIBRATIN,
+  MAV_STATE_STANDBY,
+  MAV_STATE_ACTIVE,
+  MAV_STATE_CRITICAL,
+  MAV_STATE_EMERGENCY,
+  MAV_STATE_POWEROFF,
+  MAV_STATE_FLIGHT_TERMINATION,
+};
+
 class PX4_Realsense_Bridge {
  public:
   PX4_Realsense_Bridge(const ros::NodeHandle& nh);
   ~PX4_Realsense_Bridge();
+
+  void publishSystemStatus();
 
  private:
   ros::NodeHandle nh_;
@@ -23,8 +38,13 @@ class PX4_Realsense_Bridge {
   ros::Subscriber odom_sub_;
   // Publishers
   ros::Publisher mavros_odom_pub_;
+  ros::Publisher mavros_system_status_pub_;
+
+  MAV_STATE system_status_{MAV_STATE::MAV_STATE_UNINIT};
+  MAV_STATE last_system_status_{MAV_STATE::MAV_STATE_UNINIT};
 
   void odomCallback(const nav_msgs::Odometry& msg);
+
 };
 }
 #endif  // PX4_REALSENSE_BRIDGE
---
+++ src/nodes/px4_realsense_bridge.cpp
@@ -0,0 +1,77 @@
+#include "../../include/PX4_realsense_bridge/PX4_realsense_bridge.h"
+
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <string>
+#include <thread>
+
+namespace bridge {
+
+PX4_Realsense_Bridge::PX4_Realsense_Bridge(const ros::NodeHandle& nh)
+    : nh_(nh) {
+  tf_listener_ = new tf::TransformListener(
+      ros::Duration(tf::Transformer::DEFAULT_CACHE_TIME), true);
+
+  // initialize subscribers
+  odom_sub_ = nh_.subscribe<const nav_msgs::Odometry&>(
+      "/camera/odom/sample", 1, &PX4_Realsense_Bridge::odomCallback, this);
+  // publishers
+  mavros_odom_pub_ =
+      nh_.advertise<nav_msgs::Odometry>("/mavros/odometry/out", 10);
+  mavros_system_status_pub_ =
+      nh_.advertise<mavros_msgs::CompanionProcessStatus>("/mavros/companion_process/status", 1);
+
+};
+
+PX4_Realsense_Bridge::~PX4_Realsense_Bridge() { delete tf_listener_; }
+
+
+void PX4_Realsense_Bridge::odomCallback(const nav_msgs::Odometry& msg) {
+
+  // publish odometry msg
+  nav_msgs::Odometry output = msg;
+  output.header.frame_id = "local_origin";
+  output.child_frame_id = "camera_downward";
+  mavros_odom_pub_.publish(output);
+
+  last_system_status_ = system_status_;
+
+  // check confidence in vision estimate by looking at covariance
+  if( msg.pose.covariance[0] > 0.1 ) // low confidence -> reboot companion
+  {
+    system_status_ = MAV_STATE::MAV_STATE_FLIGHT_TERMINATION;
+  }
+  else if( msg.pose.covariance[0] == 0.1 ) // medium confidence
+  {
+    system_status_ = MAV_STATE::MAV_STATE_CRITICAL;
+  }
+  else if( msg.pose.covariance[0] == 0.01 ) // high confidence
+  {
+    system_status_ = MAV_STATE::MAV_STATE_ACTIVE;
+  }
+  else
+  {
+    ROS_WARN_STREAM("Unexpected vision sensor variance");
+  }  
+
+  // publish system status immediately if it changed
+  if( last_system_status_ != system_status_ )
+  {
+    publishSystemStatus();
+  }
+
+}
+
+void PX4_Realsense_Bridge::publishSystemStatus(){
+  
+  mavros_msgs::CompanionProcessStatus status_msg;
+
+  status_msg.header.stamp = ros::Time::now();
+  status_msg.component = 197;  // MAV_COMP_ID_VISUAL_INERTIAL_ODOMETRY
+  status_msg.state = (int)system_status_;
+
+  mavros_system_status_pub_.publish(status_msg);
+}
+
+}
